a971d29262a57531e3030f306ba262eade4ee301
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-08-09 22:21:22 +0700

    Init Project

---
+++ Servlet014_HttpServlet/src/com/training/servlet/HttpServletExample.java
@@ -0,0 +1,22 @@
+package com.training.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HttpServletExample extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		String hello = "<html><body><H1>Hello World Servlet With extends Http Servlet</H1></body></html>";
+		writer.println(hello);
+	}
+}
