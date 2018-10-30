2a6e6196cea459b4b81f7c484b5feb723aa3dc11
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-10-30 13:36:13 +0700

    Init project

---
+++ Spring004_SpringBootFreeMarker/src/main/java/com/training/spring/Spring004SpringBootFreeMarkerApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring004SpringBootFreeMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring004SpringBootFreeMarkerApplication.class, args);
+	}
+}
