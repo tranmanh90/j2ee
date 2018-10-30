bae2796231e13f3612d6d156c8c5c6f8b01e53b1
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-10-30 15:14:50 +0700

    rename class

--- Spring006_SpringBootApacheTilesThymeleaf/src/main/java/com/training/spring/SpringBootApacheTilesThymeleafApplication.java
+++ Spring006_SpringBootApacheTilesThymeleaf/src/main/java/com/training/spring/SpringBootApacheTilesThymeleafApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring006SpringBootApacheTilesThymeleafApplication {
+public class SpringBootApacheTilesThymeleafApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring006SpringBootApacheTilesThymeleafApplication.class, args);
+		SpringApplication.run(SpringBootApacheTilesThymeleafApplication.class, args);
 	}
 }
--- Spring006_SpringBootApacheTilesThymeleaf/src/test/java/com/training/spring/SpringBootApacheTilesThymeleafApplicationTests.java
+++ Spring006_SpringBootApacheTilesThymeleaf/src/test/java/com/training/spring/SpringBootApacheTilesThymeleafApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Spring006SpringBootApacheTilesThymeleafApplicationTests {
+public class SpringBootApacheTilesThymeleafApplicationTests {
 
 	@Test
 	public void contextLoads() {
