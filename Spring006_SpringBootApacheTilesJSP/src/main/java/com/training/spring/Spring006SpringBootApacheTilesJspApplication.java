a6af39ee3225e01076ac2e20b989d264468c1783
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-10-30 15:09:56 +0700

    init project

---
+++ Spring006_SpringBootApacheTilesJSP/src/main/java/com/training/spring/Spring006SpringBootApacheTilesJspApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring006SpringBootApacheTilesJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring006SpringBootApacheTilesJspApplication.class, args);
+	}
+}
