ecf8e3b2c9a846b7daeb3c83af48e2fc3e88aa95
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-10-30 13:30:35 +0700

    Add classes

---
+++ Spring003_SpringBootThymeleaf/src/main/java/com/training/spring/form/PersonForm.java
@@ -0,0 +1,34 @@
+package com.training.spring.form;
+
+public class PersonForm {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("PersonForm [firstName=");
+		builder.append(firstName);
+		builder.append(", lastName=");
+		builder.append(lastName);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
