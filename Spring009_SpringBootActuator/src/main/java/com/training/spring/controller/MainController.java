cb1fe9c80aa4bc14129fbbbd1847b2da6b608ff8
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-10-31 10:18:52 +0700

    reformat

--- Spring009_SpringBootActuator/src/main/java/com/training/spring/controller/MainController.java
+++ Spring009_SpringBootActuator/src/main/java/com/training/spring/controller/MainController.java
@@ -13,18 +13,18 @@ public class MainController {
 	public String home(HttpServletRequest request) {
 		String contextPath = request.getContextPath();
 		String host = request.getServerName();
-		
+
 		// Spring boot >= 2.0.0.M7
 		String endpointBasePath = "/actuator";
 		StringBuilder sb = new StringBuilder();
 		sb.append("<h2>Sprig Boot Actuator</h2>");
 		sb.append("<ul>");
-		
+
 		// http://localhost:8090/actuator
 		String url = "http://" + host + ":8090" + contextPath + endpointBasePath;
-        sb.append("<li><a href='" + url + "'>" + url + "</a></li>");
-        sb.append("</ul>");
-		
+		sb.append("<li><a href='" + url + "'>" + url + "</a></li>");
+		sb.append("</ul>");
+
 		return sb.toString();
 	}
 }
