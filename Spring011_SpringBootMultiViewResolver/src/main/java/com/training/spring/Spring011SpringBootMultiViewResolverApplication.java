99e75e7a66ea4e8e1117a8619d2f2d1eac63aa36
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-07 11:54:38 +0700

    init project

---
+++ Spring011_SpringBootMultiViewResolver/src/main/java/com/training/spring/Spring011SpringBootMultiViewResolverApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring011SpringBootMultiViewResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring011SpringBootMultiViewResolverApplication.class, args);
+	}
+}
