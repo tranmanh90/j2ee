b0fb2359e809525c3fabc3fdfa5a18204b776f6a
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-07 09:25:31 +0700

    add language resource

---
+++ Spring010_SpringBootMultiLanguage/src/main/java/com/training/spring/Spring010SpringBootMultiLanguageApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring010SpringBootMultiLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring010SpringBootMultiLanguageApplication.class, args);
+	}
+}
