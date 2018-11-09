7e5b1824c8b6b470c980f27e4c5b6f169c38a35b
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-09 09:09:00 +0700

    init project

---
+++ Spring014_SpringBootJDBCTransaction/src/main/java/com/training/spring/Spring014SpringBootJdbcTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring014SpringBootJdbcTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring014SpringBootJdbcTransactionApplication.class, args);
+	}
+}
