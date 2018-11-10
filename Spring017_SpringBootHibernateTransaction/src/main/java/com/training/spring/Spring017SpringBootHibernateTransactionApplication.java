10223e58492285db0ad9eeadb34f83140791cb18
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-10 21:38:04 +0700

    init project

---
+++ Spring017_SpringBootHibernateTransaction/src/main/java/com/training/spring/Spring017SpringBootHibernateTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring017SpringBootHibernateTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring017SpringBootHibernateTransactionApplication.class, args);
+	}
+}
