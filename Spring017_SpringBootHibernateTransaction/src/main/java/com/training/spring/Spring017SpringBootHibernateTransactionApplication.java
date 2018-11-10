9344d499c4c0f15e6273541ccf11ff20348ea3f2
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-10 22:13:26 +0700

    disable auto configuration

--- Spring017_SpringBootHibernateTransaction/src/main/java/com/training/spring/Spring017SpringBootHibernateTransactionApplication.java
+++ Spring017_SpringBootHibernateTransaction/src/main/java/com/training/spring/Spring017SpringBootHibernateTransactionApplication.java
@@ -1,9 +1,19 @@
 package com.training.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class })
+
 public class Spring017SpringBootHibernateTransactionApplication {
 
 	public static void main(String[] args) {
