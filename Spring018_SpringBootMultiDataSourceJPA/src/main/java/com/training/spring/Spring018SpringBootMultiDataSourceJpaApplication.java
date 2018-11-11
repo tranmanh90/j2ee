0bb2f998467ec43dcc079546cb18bce1bd28a786
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-11 22:11:14 +0700

    disable spring data sources auto configure

--- Spring018_SpringBootMultiDataSourceJPA/src/main/java/com/training/spring/Spring018SpringBootMultiDataSourceJpaApplication.java
+++ Spring018_SpringBootMultiDataSourceJPA/src/main/java/com/training/spring/Spring018SpringBootMultiDataSourceJpaApplication.java
@@ -1,9 +1,20 @@
 package com.training.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+
+//Disable some Spring Boot auto config
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class })
+
 public class Spring018SpringBootMultiDataSourceJpaApplication {
 
 	public static void main(String[] args) {
