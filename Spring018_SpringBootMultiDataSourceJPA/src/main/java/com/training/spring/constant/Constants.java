255514c953d6125c8ff984da9f265f2fd84ada09
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-11 23:25:39 +0700

    modify constants

---
+++ Spring018_SpringBootMultiDataSourceJPA/src/main/java/com/training/spring/constant/Constants.java
@@ -0,0 +1,8 @@
+package com.training.spring.constant;
+
+public class Constants {
+	public static final String PACKAGE_ENTITIES_1 = "com.training.spring.entity1";
+	public static final String PACKAGE_ENTITIES_2 = "com.training.spring.entity2";
+	public static final String JPA_UNIT_NAME_1 = "PERSITENCE_UNIT_NAME_1";
+	public static final String JPA_UNIT_NAME_2 = "PERSITENCE_UNIT_NAME_2";
+}
