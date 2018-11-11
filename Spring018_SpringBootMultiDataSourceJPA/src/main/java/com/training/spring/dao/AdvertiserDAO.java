a823ac989d6f453c36f69acc774c4cdbe321d515
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-11 23:50:52 +0700

    add query statements

--- Spring018_SpringBootMultiDataSourceJPA/src/main/java/com/training/spring/dao/AdvertiserDAO.java
+++ Spring018_SpringBootMultiDataSourceJPA/src/main/java/com/training/spring/dao/AdvertiserDAO.java
@@ -1,5 +1,32 @@
 package com.training.spring.dao;
 
-public class AdvertiserDAO {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.training.spring.constant.Constants;
+import com.training.spring.entity1.Publisher;
+import com.training.spring.entity2.Advertiser;
+
+@Repository
+public class AdvertiserDAO {
+	@Autowired
+	@PersistenceContext(unitName = Constants.JPA_UNIT_NAME_2)
+	private EntityManager env;
+
+	@SuppressWarnings("unchecked")
+	public List<Publisher> listPublishers() {
+		String sql = "Select e from " + Advertiser.class.getName() + " e ";
+		Query query = env.createQuery(sql, Advertiser.class);
+		return query.getResultList();
+	}
+
+	public Advertiser findById(Long id) {
+		return this.env.find(Advertiser.class, id);
+	}
 }
--- Spring018_SpringBootMultiDataSourceJPA/src/main/java/com/training/spring/dao/PublisherDAO.java
+++ Spring018_SpringBootMultiDataSourceJPA/src/main/java/com/training/spring/dao/PublisherDAO.java
@@ -1,5 +1,31 @@
 package com.training.spring.dao;
 
-public class PublisherDAO {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.training.spring.constant.Constants;
+import com.training.spring.entity1.Publisher;
+
+@Repository
+public class PublisherDAO {
+	@Autowired
+	@PersistenceContext(unitName = Constants.JPA_UNIT_NAME_1)
+	private EntityManager env;
+
+	@SuppressWarnings("unchecked")
+	public List<Publisher> listPublishers() {
+		String sql = "Select e from " + Publisher.class.getName() + " e ";
+		Query query = env.createQuery(sql, Publisher.class);
+		return query.getResultList();
+	}
+
+	public Publisher findById(Long id) {
+		return this.env.find(Publisher.class, id);
+	}
 }
