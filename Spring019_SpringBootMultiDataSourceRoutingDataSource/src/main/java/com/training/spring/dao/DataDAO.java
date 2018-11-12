fce0b6c5cd095cb2e2d17ad0ae7594597a39fb23
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-12 13:13:35 +0700

    add data access object

---
+++ Spring019_SpringBootMultiDataSourceRoutingDataSource/src/main/java/com/training/spring/dao/DataDAO.java
@@ -0,0 +1,32 @@
+package com.training.spring.dao;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class DataDAO extends JdbcDaoSupport {
+	@Autowired
+	public DataDAO(DataSource dataSource) {
+		this.setDataSource(dataSource);
+	}
+
+	public List<String> queryPublishers() {
+		String sql = "Select name from Publishers";
+		List<String> list = this.getJdbcTemplate().queryForList(sql, String.class);
+		return list;
+	}
+
+	public List<String> queryAdvertisers() {
+		String sql = "Select name from Advertisers";
+
+		List<String> list = this.getJdbcTemplate().queryForList(sql, String.class);
+		return list;
+	}
+}
