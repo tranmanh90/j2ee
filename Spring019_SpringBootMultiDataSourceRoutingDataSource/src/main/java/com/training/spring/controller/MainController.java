861a25c227b88c900b1b0dd2acbab46442834969
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-12 13:16:20 +0700

    add controller

---
+++ Spring019_SpringBootMultiDataSourceRoutingDataSource/src/main/java/com/training/spring/controller/MainController.java
@@ -0,0 +1,42 @@
+package com.training.spring.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.training.spring.dao.DataDAO;
+
+@Controller
+public class MainController {
+	@Autowired
+	private DataDAO dataDAO;
+
+	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
+	public String home(Model model) throws SQLException {
+
+		return "home";
+	}
+
+	@RequestMapping(value = { "/advertiser/list" }, method = RequestMethod.GET)
+	public String advertiser(Model model) throws SQLException {
+
+		List<String> list = dataDAO.queryAdvertisers();
+		model.addAttribute("advertisers", list);
+
+		return "advertiser";
+	}
+
+	@RequestMapping(value = { "/publisher/list" }, method = RequestMethod.GET)
+	public String publisher(Model model) throws SQLException {
+
+		List<String> list = dataDAO.queryPublishers();
+		model.addAttribute("publishers", list);
+
+		return "publisher";
+	}
+}
