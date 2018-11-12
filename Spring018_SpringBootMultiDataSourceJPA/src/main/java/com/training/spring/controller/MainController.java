1f70476e38d1fd56ce37763f3859f0e0f52cff13
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-12 08:31:15 +0700

    add controller

---
+++ Spring018_SpringBootMultiDataSourceJPA/src/main/java/com/training/spring/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.training.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.training.spring.dao.AdvertiserDAO;
+import com.training.spring.dao.PublisherDAO;
+import com.training.spring.entity1.Publisher;
+import com.training.spring.entity2.Advertiser;
+
+@Controller
+public class MainController {
+	@Autowired
+	private PublisherDAO publisherDAO;
+
+	@Autowired
+	private AdvertiserDAO advertiserDAO;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String homepage(Model model) {
+		List<Advertiser> advertisers = advertiserDAO.listAdvertisers();
+		List<Publisher> publishers = publisherDAO.listPublishers();
+
+		model.addAttribute("advertisers", advertisers);
+		model.addAttribute("publishers", publishers);
+
+		return "home";
+	}
+}
