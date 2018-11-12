93548666b033432725817ac67d19a8386c9f3e08
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-12 09:52:26 +0700

    init

---
+++ Spring019_SpringBootMultiDataSourceRoutingDataSource/src/main/java/com/training/spring/Spring019SpringBootMultiDataSourceRoutingDataSourceApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring019SpringBootMultiDataSourceRoutingDataSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring019SpringBootMultiDataSourceRoutingDataSourceApplication.class, args);
+	}
+}
