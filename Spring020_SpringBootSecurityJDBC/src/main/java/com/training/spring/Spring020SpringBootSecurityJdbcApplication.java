3da9e3a0c83b31ac88391db995c151aca4730b27
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-12 15:04:28 +0700

    init

---
+++ Spring020_SpringBootSecurityJDBC/src/main/java/com/training/spring/Spring020SpringBootSecurityJdbcApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring020SpringBootSecurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring020SpringBootSecurityJdbcApplication.class, args);
+	}
+}
