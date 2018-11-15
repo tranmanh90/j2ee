f5f1e2561f34eb0990b050fca4dd62a048464136
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-15 13:08:08 +0700

    init

---
+++ Spring025_SpringBootCRUDRestfulWebService/src/main/java/com/training/spring/Spring025SpringBootCrudRestfulWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring025SpringBootCrudRestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring025SpringBootCrudRestfulWebServiceApplication.class, args);
+	}
+}
