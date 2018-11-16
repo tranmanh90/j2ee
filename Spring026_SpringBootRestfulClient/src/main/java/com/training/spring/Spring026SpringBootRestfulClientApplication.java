d1ffbd49214a2a7d1e4b6826c3649cd8910d00f3
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-16 13:21:18 +0700

    init

---
+++ Spring026_SpringBootRestfulClient/src/main/java/com/training/spring/Spring026SpringBootRestfulClientApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring026SpringBootRestfulClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring026SpringBootRestfulClientApplication.class, args);
+	}
+}
