08e48b09f0632ba6f44140609134e7a01cae43bc
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-16 13:12:36 +0700

    add classes

---
+++ Spring025_SpringBootCRUDRestfulWebService/src/main/java/com/training/spring/mapper/EmployeeMapper.java
@@ -0,0 +1,22 @@
+package com.training.spring.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.training.spring.model.Employee;
+
+public class EmployeeMapper implements RowMapper<Employee> {
+
+	public static final String BASE_SQL = "select emp.emp_no, emp.emp_name, emp.emp_position from employee emp";
+
+	@Override
+	public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+		String empNo = rs.getString("EMP_NO");
+		String empName = rs.getString("EMP_NAME");
+		String empPosition = rs.getString("EMP_POSITION");
+		return new Employee(empNo, empName, empPosition);
+	}
+
+}
