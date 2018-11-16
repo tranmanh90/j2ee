6a4a0daaa0c0dcda772b9175c06619d92bdd1368
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-16 15:32:47 +0700

    init

---
+++ Spring027_SpringBootRESTfulServiceBasicAuthentication/src/main/java/com/training/spring/Spring027SpringBootResTfulServiceBasicAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring027SpringBootResTfulServiceBasicAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring027SpringBootResTfulServiceBasicAuthenticationApplication.class, args);
+	}
+}
