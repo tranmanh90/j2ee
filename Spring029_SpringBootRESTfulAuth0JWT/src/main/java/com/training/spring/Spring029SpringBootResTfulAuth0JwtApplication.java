4021c7bcd8453b5ab536a196a8218d18d5bc47fd
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-21 11:03:05 +0700

    init

---
+++ Spring029_SpringBootRESTfulAuth0JWT/src/main/java/com/training/spring/Spring029SpringBootResTfulAuth0JwtApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring029SpringBootResTfulAuth0JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring029SpringBootResTfulAuth0JwtApplication.class, args);
+	}
+}
