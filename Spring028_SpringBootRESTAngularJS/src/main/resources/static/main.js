13625cf2fbcaa9d857fe33324e1b656ded37ad77
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-21 10:59:17 +0700

    add classes

--- Spring028_SpringBootRESTAngularJS/src/main/resources/static/main.js
+++ Spring028_SpringBootRESTAngularJS/src/main/resources/static/main.js
@@ -1,1 +1,104 @@
-@charset "ISO-8859-1";
+var app = angular.module("EmployeeManagement", []);
+var isCreated;
+
+// Controller Part
+app.controller("EmployeeController", function($scope, $http) {
+
+	isCreated = true;
+	$scope.employees = [];
+	$scope.employeeForm = {
+		//empId : 1,
+		empNo : "",
+		empName : "",
+		position : ""
+	};
+
+	// Now load the data from server
+	_refreshEmployeeData();
+
+	// HTTP POST/PUT methods for add/edit employee
+	// Call: http://localhost:8080/employee
+	$scope.submitEmployee = function() {
+
+		var method = "";
+		var url = "";
+
+		//if ($scope.employeeForm.empId == -1) {
+		if (isCreated) {
+			isCreated = true;
+			method = "POST";
+			url = '/employee/create';
+		} else {
+			method = "PUT";
+			url = '/employee/update';
+		}
+
+		$http({
+			method : method,
+			url : url,
+			data : angular.toJson($scope.employeeForm),
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}).then(_success, _error);
+	};
+
+	$scope.createEmployee = function() {
+		_clearFormData();
+	}
+
+	// HTTP DELETE- delete employee by Id
+	// Call: http://localhost:8080/employee/{empId}
+	$scope.deleteEmployee = function(employee) {
+		$http({
+			method : 'DELETE',
+			url : '/employee/delete/' + employee.empNo
+		}).then(_success, _error);
+	};
+
+	// In case of edit
+	$scope.editEmployee = function(employee) {
+		//$scope.employeeForm.empId = employee.empId;
+		isCreated = false;
+		$scope.employeeForm.empNo = employee.empNo;
+		$scope.employeeForm.empName = employee.empName;
+		$scope.employeeForm.position = employee.position;
+	};
+
+	// Private Method
+	// HTTP GET- get all employees collection
+	// Call: http://localhost:8080/employees
+	function _refreshEmployeeData() {
+		$http({
+			method : 'GET',
+			url : '/employees'
+		}).then(function(res) { // success
+			$scope.employees = res.data;
+		}, function(res) { // error
+			console.log("Error: " + res.status + " : " + res.data);
+		});
+	}
+
+	function _success(res) {
+		_refreshEmployeeData();
+		_clearFormData();
+	}
+
+	function _error(res) {
+		var data = res.data;
+		var status = res.status;
+		var header = res.header;
+		var config = res.config;
+		alert("Error: " + status + ":" + data);
+	}
+
+	// Clear the form
+	function _clearFormData() {
+		//$scope.employeeForm.empId = -1;
+		isCreated = true;
+		$scope.employeeForm.empNo = "";
+		$scope.employeeForm.empName = "";
+		$scope.employeeForm.position = "";
+	}
+	;
+});
