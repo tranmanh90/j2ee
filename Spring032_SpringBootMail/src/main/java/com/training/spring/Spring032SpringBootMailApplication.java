271af789811e4c00d21b7c216782e3c4987b67f1
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-24 00:31:29 +0700

    init

---
+++ Spring032_SpringBootMail/src/main/java/com/training/spring/Spring032SpringBootMailApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring032SpringBootMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring032SpringBootMailApplication.class, args);
+	}
+}
