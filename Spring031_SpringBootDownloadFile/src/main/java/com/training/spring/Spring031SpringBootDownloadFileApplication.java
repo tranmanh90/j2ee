72ce5dfc79cdd9e2dbd2cf6abe1c92d266510cbf
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-23 23:29:53 +0700

    init

---
+++ Spring031_SpringBootDownloadFile/src/main/java/com/training/spring/Spring031SpringBootDownloadFileApplication.java
@@ -0,0 +1,12 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring031SpringBootDownloadFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring031SpringBootDownloadFileApplication.class, args);
+	}
+}
