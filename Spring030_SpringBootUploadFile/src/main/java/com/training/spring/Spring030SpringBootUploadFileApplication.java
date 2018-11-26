2b0657d46a3c517015c8d68a2e54b3a61381d482
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-26 14:17:00 +0700

    config to deploy on tomcat server

--- Spring030_SpringBootUploadFile/src/main/java/com/training/spring/Spring030SpringBootUploadFileApplication.java
+++ Spring030_SpringBootUploadFile/src/main/java/com/training/spring/Spring030SpringBootUploadFileApplication.java
@@ -2,9 +2,17 @@ package com.training.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Spring030SpringBootUploadFileApplication {
+public class Spring030SpringBootUploadFileApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(Spring030SpringBootUploadFileApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring030SpringBootUploadFileApplication.class, args);
