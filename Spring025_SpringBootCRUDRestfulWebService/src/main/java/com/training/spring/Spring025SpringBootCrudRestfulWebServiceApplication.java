52ed4e08ab015e7b91a5d699e929de2f55c26b32
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-11-29 16:31:41 +0700

    configure to deploy on tomcat server

--- Spring025_SpringBootCRUDRestfulWebService/src/main/java/com/training/spring/Spring025SpringBootCrudRestfulWebServiceApplication.java
+++ Spring025_SpringBootCRUDRestfulWebService/src/main/java/com/training/spring/Spring025SpringBootCrudRestfulWebServiceApplication.java
@@ -2,9 +2,17 @@ package com.training.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Spring025SpringBootCrudRestfulWebServiceApplication {
+public class Spring025SpringBootCrudRestfulWebServiceApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(Spring025SpringBootCrudRestfulWebServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring025SpringBootCrudRestfulWebServiceApplication.class, args);
