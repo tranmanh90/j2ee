6330926092c4a2fe3093f6dda0178431e44d110c
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-05 11:21:56 +0700

    add constant class

---
+++ FreeBookStore/src/main/java/com/book/store/constant/BTConstants.java
@@ -0,0 +1,13 @@
+package com.book.store.constant;
+
+public class BTConstants {
+	public static final class RESPONSE {
+		public static final String TRN_SUCC = "000";
+		public static final String TRN_NOTF = "001";
+		
+		public static final String TRN_MSG_SUCC = "Succeed !!!";
+		public static final String TRN_MSG_NOTF = "Data not found";
+	}
+	
+	public static final String COVER_IMAGE_PATH = "D:/Document/Media/cover/";
+}
