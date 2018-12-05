80360c4419f7069580f606c4ab3828aef19a93c5
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-05 23:45:25 +0700

    add logic code

--- FreeBookStore/src/main/java/com/book/store/controller/ServingImageRESTController.java
+++ FreeBookStore/src/main/java/com/book/store/controller/ServingImageRESTController.java
@@ -3,25 +3,38 @@ package com.book.store.controller;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.book.store.constant.BTConstants;
+import com.book.store.dao.CoverImageDAO;
+import com.book.store.model.CoverImage;
 
 @RestController
 public class ServingImageRESTController {
-	@RequestMapping(value = "/image1", method = RequestMethod.GET)
-	public ResponseEntity<byte[]> getImage1() throws IOException {
-		RandomAccessFile f = new RandomAccessFile(BTConstants.COVER_IMAGE_LOCAL_PATH + "", "r");
-		byte[] b = new byte[(int) f.length()];
-		f.readFully(b);
+	
+	@Autowired
+	private CoverImageDAO coverImageDAO;
+	
+	@RequestMapping(value = "/image/{urlId}", method = RequestMethod.GET)
+	public ResponseEntity<byte[]> getCoverImage(@PathVariable("urlId") String urlId) throws IOException {
+		CoverImage coverImage = coverImageDAO.getCoverImage(urlId);
 		final HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.IMAGE_JPEG);
-		return new ResponseEntity<byte[]>(b, headers, HttpStatus.CREATED);
+		if(coverImage != null) {
+			/* "D:/Document/Media/cover" + "/" + "CV0000000000" + "." + "png" */
+			RandomAccessFile f = new RandomAccessFile(coverImage.getImagePath() + "/" + coverImage.getImageName() + "." + coverImage.getImageFormat(), "r");
+			byte[] b = new byte[(int) f.length()];
+			f.readFully(b);
+			headers.setContentType(MediaType.IMAGE_JPEG);
+			return new ResponseEntity<byte[]>(b, headers, HttpStatus.CREATED);
+		}
+		return new ResponseEntity<byte[]>(HttpStatus.NOT_FOUND);
 	}
 }
