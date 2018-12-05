3b16eaff8766b7cb8bc912797aeac513d6f1162f
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-05 15:00:51 +0700

    add function to query data

--- FreeBookStore/src/main/java/com/book/store/dao/CoverImageDAO.java
+++ FreeBookStore/src/main/java/com/book/store/dao/CoverImageDAO.java
@@ -1,5 +1,53 @@
 package com.book.store.dao;
 
-public class CoverImageDAO {
+import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import com.book.store.mapper.AuthorMapper;
+import com.book.store.mapper.CoverImageMapper;
+import com.book.store.model.Author;
+import com.book.store.model.CoverImage;
+
+@Repository
+public class CoverImageDAO extends JdbcDaoSupport {
+
+	private static final String BASE_SQL = "SELECT CI.IMAGE_ID, CI.IMAGE_URL, CI.IMAGE_PATH, CI.IMAGE_NAME FROM COVER_IMAGES CI";
+
+	@Autowired
+	public CoverImageDAO(DataSource dataSource) {
+		this.setDataSource(dataSource);
+	}
+
+	// Read (GET method)
+	public CoverImage getCoverImage(String imageId) {
+		String sql = BASE_SQL + " WHERE CI.IMAGE_ID = ?";
+		Object[] params = new Object[] { imageId };
+		CoverImageMapper mapper = new CoverImageMapper();
+		try {
+			CoverImage coverImage = this.getJdbcTemplate().queryForObject(sql, params, mapper);
+			return coverImage;
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
+
+	public List<CoverImage> getListCoverImage() {
+		String sql = BASE_SQL;
+		Object[] params = new Object[] {};
+		CoverImageMapper mapper = new CoverImageMapper();
+		List<CoverImage> listImages = this.getJdbcTemplate().query(sql, params, mapper);
+		return listImages;
+	}
+
+	// Update (PUT method)
+
+	// Create (POST method)
+
+	// Delete (DELETE method)
 }
