0e4c98cd5076355fa387dd7ae29c5a23a8b1a6fc
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-05 14:13:22 +0700

    add response infor

--- FreeBookStore/src/main/java/com/book/store/model/BookDetails.java
+++ FreeBookStore/src/main/java/com/book/store/model/BookDetails.java
@@ -4,22 +4,45 @@ import java.util.List;
 
 public class BookDetails {
 	private List<Book> books;
+	private String responseMessage;
+	private String responseCode;
 
 	public List<Book> getBooks() {
 		return books;
 	}
-	
-	
 
-	public BookDetails(List<Book> books) {
+	public BookDetails(List<Book> books, String responseCode,String responseMessage) {
 		super();
 		this.books = books;
+		this.responseMessage = responseMessage;
+		this.responseCode = responseCode;
+		
 	}
-
-
+	
+	public BookDetails(String responseCode, String responseMessage) {
+		super();
+		this.responseMessage = responseMessage;
+		this.responseCode = responseCode;
+	}
 
 	public void setBooks(List<Book> books) {
 		this.books = books;
+	}
+
+	public String getResponseMessage() {
+		return responseMessage;
+	}
+
+	public void setResponseMessage(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}
+
+	public String getResponseCode() {
+		return responseCode;
+	}
+
+	public void setResponseCode(String responseCode) {
+		this.responseCode = responseCode;
 	}
 
 	@Override
@@ -27,6 +50,10 @@ public class BookDetails {
 		StringBuilder builder = new StringBuilder();
 		builder.append("BookDetails [books=");
 		builder.append(books);
+		builder.append(", responseMessage=");
+		builder.append(responseMessage);
+		builder.append(", responseCode=");
+		builder.append(responseCode);
 		builder.append("]");
 		return builder.toString();
 	}
