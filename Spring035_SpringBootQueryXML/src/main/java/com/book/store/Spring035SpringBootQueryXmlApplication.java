7423338c1ea6a91dc4a063c920989d80cffe36cc
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-07 16:18:45 +0700

    init project

---
+++ Spring035_SpringBootQueryXML/src/main/java/com/book/store/Spring035SpringBootQueryXmlApplication.java
@@ -0,0 +1,12 @@
+package com.book.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring035SpringBootQueryXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring035SpringBootQueryXmlApplication.class, args);
+	}
+}
