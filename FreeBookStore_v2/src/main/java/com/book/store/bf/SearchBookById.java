ea892567ae8dec16bdd2c7bc0b70c6ee757b1a7d
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-23 00:32:28 +0700

    add bf for search book by id feature

---
+++ FreeBookStore_v2/src/main/java/com/book/store/bf/SearchBookById.java
@@ -0,0 +1,40 @@
+package com.book.store.bf;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.book.store.df.Dbook;
+import com.book.store.vo.Book00VO;
+
+/**************************************************************
+ * <pre>
+* Search by book Id
+ * </pre>
+ * 
+ * @author TRAN VAN MANH
+ * @email tranmanh.vn90@gmail.com
+ * @importance
+ *************************************************************/
+public class SearchBookById {
+	private String className = getClass().getName();
+	private Logger logger = LoggerFactory.getLogger(className);
+
+	private Dbook df = new Dbook();
+
+	/**************************************************************
+	 * <pre>
+	* Execute search action
+	 * </pre>
+	 * 
+	 * @param vo request data from client
+	 * @return A searched books
+	 *************************************************************/
+	public List<Book00VO> execute(Book00VO vo) {
+		logger.info("");
+		List<Book00VO> returnList = null;
+		returnList = df.s001(vo);
+		return returnList;
+	}
+}
