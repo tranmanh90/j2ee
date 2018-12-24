d26ef60744eb9c0d1a776bb3a4e8d081f9980ac3
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-24 16:51:45 +0700

    add fields for request entity

--- FreeBookStore_v2/src/main/java/com/book/store/dto/Author00Request.java
+++ FreeBookStore_v2/src/main/java/com/book/store/dto/Author00Request.java
@@ -3,5 +3,45 @@ package com.book.store.dto;
 import com.book.store.BaseRequest;
 
 public class Author00Request extends BaseRequest {
+	private int page;
+	private int rowPerPage;
+	private String authorName;
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public int getRowPerPage() {
+		return rowPerPage;
+	}
+
+	public void setRowPerPage(int rowPerPage) {
+		this.rowPerPage = rowPerPage;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Author00Request [page=");
+		builder.append(page);
+		builder.append(", rowPerPage=");
+		builder.append(rowPerPage);
+		builder.append(", authorName=");
+		builder.append(authorName);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
