0a6cd8552ae82294c11f4a0499c6b0a2601c37da
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-24 16:48:14 +0700

    add transaction main

--- FreeBookStore_v2/src/main/java/com/book/store/tm/TMAuthor00.java
+++ FreeBookStore_v2/src/main/java/com/book/store/tm/TMAuthor00.java
@@ -1,13 +1,40 @@
 package com.book.store.tm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.book.store.BaseRespone;
+import com.book.store.IBasePM;
 import com.book.store.IBaseTM;
 import com.book.store.dto.Author00Request;
 import com.book.store.dto.Author00Response;
+import com.book.store.pm.PMAuthor00;
 
 @RestController
 public class TMAuthor00 implements IBaseTM<Author00Request, Author00Response> {
+	private String className = getClass().getName();
+	private Logger logger = LoggerFactory.getLogger(className);
+	private IBasePM<Author00Request, Author00Response> pm = new PMAuthor00();
+
+	@RequestMapping(value = "/freebookstore/api/v2/search-author-by-name", //
+			method = RequestMethod.POST, //
+			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@ResponseBody
+	public BaseRespone[] execute(@RequestBody Author00Request request) {
+		Author00Response response = pm.execute(request);
+		List<BaseRespone> resultDto = new ArrayList<>();
+		resultDto.add(response);
+		return resultDto.toArray(new BaseRespone[resultDto.size()]);
+	}
 
 	@Override
 	public void postProcess(Author00Request request, Author00Response response) {
