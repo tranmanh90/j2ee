ead5b53cdff3bb3277203d04ace8d3bc12f023bb
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-24 15:10:33 +0700

    Add searchAuthorByName

--- FreeBookStore_v2/src/main/java/com/book/store/common/IBDBookStore.java
+++ FreeBookStore_v2/src/main/java/com/book/store/common/IBDBookStore.java
@@ -3,13 +3,23 @@ package com.book.store.common;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.book.store.bf.SearchAuthorByName;
 import com.book.store.bf.SearchBookById;
 import com.book.store.bf.SearchBookByTitle;
+import com.book.store.vo.Author00VO;
 import com.book.store.vo.Book00VO;
 import com.book.store.vo.Book01VO;
 
 public class IBDBookStore implements IIBookStore {
 
+	/**************************************************************
+	 * <pre>
+	* Search book by title
+	 * </pre>
+	 * 
+	 * @param vo request data from client
+	 * @return List of searched book
+	 *************************************************************/
 	@Override
 	public List<Book00VO> searchBookByTitle(Book00VO vo) {
 		SearchBookByTitle searchBookByTitle = new SearchBookByTitle();
@@ -18,12 +28,36 @@ public class IBDBookStore implements IIBookStore {
 		return listBook;
 	}
 
+	/**************************************************************
+	 * <pre>
+	* Search book by id
+	 * </pre>
+	 * 
+	 * @param vo request data from client
+	 * @return The searched book
+	 *************************************************************/
 	@Override
 	public List<Book01VO> searchBookById(Book01VO vo) {
 		SearchBookById searchBookById = new SearchBookById();
 		List<Book01VO> listBook = new ArrayList<>();
 		listBook = searchBookById.execute(vo);
 		return listBook;
+	}
+
+	/**************************************************************
+	 * <pre>
+	* Search author by name
+	 * </pre>
+	 * 
+	 * @param vo request data from client
+	 * @return The searched author
+	 *************************************************************/
+	@Override
+	public List<Author00VO> searchAuthorByName(Author00VO vo) {
+		SearchAuthorByName searchAuthorByName = new SearchAuthorByName();
+		List<Author00VO> listAuthor = new ArrayList<>();
+		listAuthor = searchAuthorByName.execute(vo);
+		return listAuthor;
 	}
 
 }
