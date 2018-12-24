00fdd77fe1902adab24dc2ea213353ef4eeb3a67
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-24 14:55:15 +0700

    add s000: search author by name

--- FreeBookStore_v2/src/main/java/com/book/store/df/Dauthor.java
+++ FreeBookStore_v2/src/main/java/com/book/store/df/Dauthor.java
@@ -1,10 +1,13 @@
 package com.book.store.df;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.book.store.config.ConfigDataSource;
 import com.book.store.query.SqlLoader;
@@ -33,9 +36,34 @@ public class Dauthor {
 	 * @param vo request data from client
 	 * @return The searched author
 	 *************************************************************/
-//	public List<Author00VO> s000(Author00VO vo){
-//		String methodName = "s000";
-//		logger.info(className + ": " + methodName);
-//		String query = "AUTHORS.s000";
-//	}
+	public List<Author00VO> s000(Author00VO vo) {
+		String methodName = "s000";
+		logger.info(className + ": " + methodName);
+		String query = "AUTHORS.s000";
+
+		Object[] pMapper = new Object[] { vo.getPage(), vo.getRowPerPage(), vo.getAuthorName() };
+
+		RowMapper<Author00VO> rMapper = new RowMapper<Author00VO>() {
+
+			@Override
+			public Author00VO mapRow(ResultSet rs, int rowNum) throws SQLException {
+				String tempString = "";
+				Author00VO returnVO = new Author00VO();
+
+				returnVO.setTotalPages(rs.getInt("TOTAL_PAGES"));
+				returnVO.setTotalRows(rs.getInt("TOTAL_ROWS"));
+				returnVO.setPage(rs.getInt("PAGE_INDEX"));
+
+				tempString = rs.getString("AUTHOR_ID");
+				returnVO.setAuthorId(tempString == null ? null : tempString.trim());
+				tempString = rs.getString("AUTHOR_NAME");
+				returnVO.setAuthorName(tempString == null ? null : tempString.trim());
+				tempString = rs.getString("AUTHOR_ABOUT");
+				returnVO.setAuthorAbout(tempString == null ? null : tempString.trim());
+
+				return returnVO;
+			}
+		};
+		return jdbc.query(loader.getSql(query), pMapper, rMapper);
+	}
 }
