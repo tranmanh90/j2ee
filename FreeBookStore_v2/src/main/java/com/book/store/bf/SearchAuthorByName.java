9243918aabf44e979053b1899a372621940e217a
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-24 15:09:56 +0700

    add logic code

--- FreeBookStore_v2/src/main/java/com/book/store/bf/SearchAuthorByName.java
+++ FreeBookStore_v2/src/main/java/com/book/store/bf/SearchAuthorByName.java
@@ -1,5 +1,20 @@
 package com.book.store.bf;
 
+import java.util.List;
+
+import com.book.store.df.Dauthor;
+import com.book.store.vo.Author00VO;
+
 public class SearchAuthorByName {
 
+	private Dauthor df = null;
+
+	public List<Author00VO> execute(Author00VO vo) {
+		List<Author00VO> returnList = null;
+		if (df == null) {
+			df = new Dauthor();
+		}
+		returnList = df.s000(vo);
+		return returnList;
+	}
 }
