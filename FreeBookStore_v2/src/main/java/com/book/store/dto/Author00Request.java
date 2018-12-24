700bc67c1ad47df3f38021ccc91bcc18a6d2ad72
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-24 14:25:17 +0700

    add author dto

---
+++ FreeBookStore_v2/src/main/java/com/book/store/dto/Author00Request.java
@@ -0,0 +1,7 @@
+package com.book.store.dto;
+
+import com.book.store.BaseRequest;
+
+public class Author00Request extends BaseRequest {
+
+}
---
+++ FreeBookStore_v2/src/main/java/com/book/store/dto/Author00Response.java
@@ -0,0 +1,7 @@
+package com.book.store.dto;
+
+import com.book.store.BaseRespone;
+
+public class Author00Response extends BaseRespone {
+
+}
