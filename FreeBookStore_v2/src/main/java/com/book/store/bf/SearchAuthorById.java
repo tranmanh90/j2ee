70e043374ca22a163ceba32f7c327aadbf87bdd8
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-25 11:34:07 +0700

    add logic for bf

--- FreeBookStore_v2/src/main/java/com/book/store/bf/SearchAuthorById.java
+++ FreeBookStore_v2/src/main/java/com/book/store/bf/SearchAuthorById.java
@@ -1,5 +1,36 @@
 package com.book.store.bf;
 
-public class SearchAuthorById {
+import java.util.List;
 
+import com.book.store.df.Dauthor;
+import com.book.store.vo.Author00VO;
+
+/**************************************************************
+ * <pre>
+* Search author by id BF
+ * </pre>
+ * 
+ * @author TRAN VAN MANH
+ * @email tranmanh.vn90@gmail.com
+ * @importance
+ *************************************************************/
+public class SearchAuthorById {
+	private Dauthor df = null;
+
+	/**************************************************************
+	 * <pre>
+	* Execute query author by id
+	 * </pre>
+	 * 
+	 * @param request data object from client
+	 * @return Author array
+	 *************************************************************/
+	public List<Author00VO> execute(Author00VO vo) {
+		List<Author00VO> returnList = null;
+		if (df == null) {
+			df = new Dauthor();
+		}
+		returnList = df.s001(vo);
+		return returnList;
+	}
 }
--- FreeBookStore_v2/src/main/java/com/book/store/bf/SearchAuthorByName.java
+++ FreeBookStore_v2/src/main/java/com/book/store/bf/SearchAuthorByName.java
@@ -5,10 +5,27 @@ import java.util.List;
 import com.book.store.df.Dauthor;
 import com.book.store.vo.Author00VO;
 
+/**************************************************************
+ * <pre>
+* Search author by name BF
+ * </pre>
+ * 
+ * @author TRAN VAN MANH
+ * @email tranmanh.vn90@gmail.com
+ * @importance
+ *************************************************************/
 public class SearchAuthorByName {
 
 	private Dauthor df = null;
 
+	/**************************************************************
+	 * <pre>
+	* Execute query author by name
+	 * </pre>
+	 * 
+	 * @param request data object from client
+	 * @return Author array
+	 *************************************************************/
 	public List<Author00VO> execute(Author00VO vo) {
 		List<Author00VO> returnList = null;
 		if (df == null) {
