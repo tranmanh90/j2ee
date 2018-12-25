b544e0eed9a343fa41bd735d92c64ac0ce8eb8a6
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-25 16:38:25 +0700

    add logic

--- FreeBookStore_v2/src/main/java/com/book/store/bf/SearchBookByAuthor.java
+++ FreeBookStore_v2/src/main/java/com/book/store/bf/SearchBookByAuthor.java
@@ -1,5 +1,39 @@
 package com.book.store.bf;
 
-public class SearchBookByAuthor {
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.book.store.df.Dbook;
+import com.book.store.vo.Book00VO;
+
+/**************************************************************
+ * <pre>
+* Search book by author id
+ * </pre>
+ * 
+ * @author TRAN VAN MANH
+ * @email tranmanh.vn90@gmail.com
+ * @importance
+ *************************************************************/
+public class SearchBookByAuthor {
+	private String className = getClass().getName();
+	private Logger logger = LoggerFactory.getLogger(className);
+
+	private Dbook df = new Dbook();
+
+	/**************************************************************
+	 * <pre>
+	* Execute search action
+	 * </pre>
+	 * 
+	 * @param vo request data from client
+	 * @return A searched books
+	 *************************************************************/
+	public List<Book00VO> execute(Book00VO vo) {
+		List<Book00VO> returnList = null;
+		returnList = df.s002(vo);
+		return returnList;
+	}
 }
