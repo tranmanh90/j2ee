20f6568068c75db626b9ce4f0288eec210a92cad
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-26 22:47:33 +0700

    add search by containing words bf

--- FreeBookStore_v2/src/main/java/com/book/store/bf/SearchBookByContainingWords.java
+++ FreeBookStore_v2/src/main/java/com/book/store/bf/SearchBookByContainingWords.java
@@ -1,5 +1,38 @@
 package com.book.store.bf;
 
-public class SearchBookByContainingWords {
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.book.store.df.Dbook;
+import com.book.store.vo.Book04VO;
+
+/**************************************************************
+ * <pre>
+* Search book by containing words
+ * </pre>
+ * 
+ * @author TRAN VAN MANH
+ * @email tranmanh.vn90@gmail.com
+ * @importance
+ *************************************************************/
+public class SearchBookByContainingWords {
+	private String className = getClass().getName();
+	private Logger logger = LoggerFactory.getLogger(className);
+	private Dbook df = new Dbook();
+
+	/**************************************************************
+	 * <pre>
+	* Execute search action
+	 * </pre>
+	 * 
+	 * @param vo request data from client
+	 * @return A searched books
+	 *************************************************************/
+	public List<Book04VO> execute(Book04VO vo) {
+		List<Book04VO> returnList = null;
+		returnList = df.s004(vo);
+		return returnList;
+	}
 }
