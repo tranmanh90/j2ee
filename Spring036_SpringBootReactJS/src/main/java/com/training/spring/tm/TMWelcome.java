8d7cef362b59cee754c020f3cbf1f2aa6fc37ef0
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-28 18:13:56 +0700

    add welcome transaction main

---
+++ Spring036_SpringBootReactJS/src/main/java/com/training/spring/tm/TMWelcome.java
@@ -0,0 +1,14 @@
+package com.training.spring.tm;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TMWelcome {
+	@RequestMapping("/")
+	@ResponseBody
+	public String welcome() {
+		return "Welcome to Spring Boot and ReactJS";
+	}
+}
