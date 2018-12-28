326f9380d061288c704b5a3e462385c63d19950d
tranmanh90 <31216001+tranmanh90@users.noreply.github.com>
2018-12-28 18:07:22 +0700

    init project

---
+++ Spring036_SpringBootReactJS/src/main/java/com/training/spring/Spring036SpringBootReactJsApplication.java
@@ -0,0 +1,14 @@
+package com.training.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring036SpringBootReactJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring036SpringBootReactJsApplication.class, args);
+	}
+
+}
+
